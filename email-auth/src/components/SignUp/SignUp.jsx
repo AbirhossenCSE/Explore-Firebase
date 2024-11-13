fa7beca1fcd01b1e48e36a9345149f2012520e3f
Abir Hossen <abir.cse4@gmail.com>
2024-11-13 20:21:49 +0600

    show password and accept terms

--- email-auth/src/components/SignUp/SignUp.jsx
+++ email-auth/src/components/SignUp/SignUp.jsx
@@ -1,20 +1,29 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase.init';
 import { useState } from 'react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const SignUp = () => {
     const [success, setSuccess] = useState(false);
     const [errorMessagr, setErrorMessage] = useState('');
+    const [showPassword, setShoePassword] = useState(false);
 
-    const handleSignUp = e =>{
+
+    const handleSignUp = e => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email, password);
+        const terms = e.target.terms.checked;
+        console.log(email, password, terms);
 
         //reset error message
         setErrorMessage('');
         setSuccess(false);
+
+        if (!terms) {
+            setErrorMessage('Please Accept Our Terms');
+            return;
+        }
 
         // password
         if (password.length < 6) {
@@ -30,47 +39,63 @@ const SignUp = () => {
 
         // Create user
         createUserWithEmailAndPassword(auth, email, password)
-        .then(result =>{
-            console.log(result.user);
-            setSuccess(true);
-        })
-        .catch(error =>{
-            console.log("ERROR", error); 
-            setErrorMessage(error.message);
-            setSuccess(false);          
-        })
+            .then(result => {
+                console.log(result.user);
+                setSuccess(true);
+            })
+            .catch(error => {
+                console.log("ERROR", error);
+                setErrorMessage(error.message);
+                setSuccess(false);
+            })
     }
 
     return (
-                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto mt-5">
-                    <h1 className='text-3xl font-bold mx-auto my-3'>Sign-Up Now</h1>
-                    <form onSubmit={handleSignUp} className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary">Sign-Up</button>
-                        </div>
-                    </form>
-                    {
-                        errorMessagr && <p className='text-red-600'>{errorMessagr}</p>
-                    }
-                    {
-                        success && <p className='text-green-600'>Successfully Signed-Up</p>
-                    }
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto mt-5">
+            <h1 className='text-3xl font-bold mx-auto my-3'>Sign-Up Now</h1>
+            <form onSubmit={handleSignUp} className="card-body">
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" name='email' placeholder="email" className="input input-bordered" required />
                 </div>
+                <div className="form-control relative">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    {/* password show */}
+                    <input type={showPassword ? 'text' : 'password'} name='password' placeholder="password" className="input input-bordered" required />
+                    
+                    <button onClick={() => setShoePassword(!showPassword)} className='btn btn-xs absolute right-2 top-12'>
+                        {
+                            showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                        }
+                    </button>
+
+                    <label className="label">
+                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                    </label>
+                </div>
+
+                <div className="justify-start">
+                    <label className="label cursor-pointer justify-start">
+                        <input type="checkbox" name='terms' className="checkbox mr-1" />
+                        <span className="label-text">Accept Terms & Conditions</span>
+                    </label>
+                </div>
+
+                <div className="form-control mt-6">
+                    <button className="btn btn-primary">Sign-Up</button>
+                </div>
+            </form>
+            {
+                errorMessagr && <p className='text-red-600'>{errorMessagr}</p>
+            }
+            {
+                success && <p className='text-green-600'>Successfully Signed-Up</p>
+            }
+        </div>
     );
 };
 
