b83e50c3f79f496795feb8495a619de2198ab0c7
Abir Hossen <abir.cse4@gmail.com>
2024-11-16 19:31:34 +0600

    auth state changed

--- milon-mela/src/components/Login.jsx
+++ milon-mela/src/components/Login.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProviders';
 
 const Login = () => {
+
+    const {signInUser} = useContext(AuthContext)
 
     const handleLogin = e =>{
         e.preventDefault();
@@ -9,6 +12,15 @@ const Login = () => {
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email, password);
+
+        // signin
+        signInUser(email, password)
+        .then(result =>{
+            console.log(result.user)
+        })
+        .catch(error =>{
+            console.log('ERROR', error.message)
+        })
         
     }
 
