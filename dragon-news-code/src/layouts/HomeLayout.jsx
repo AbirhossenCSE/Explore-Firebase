8226571804624ea688ba2b9cc5d773b1e9f30d16
Abir Hossen <abir.cse4@gmail.com>
2024-11-18 14:55:34 +0600

    Fetch and Show Category Data

--- dragon-news-code/src/layouts/HomeLayout.jsx
+++ dragon-news-code/src/layouts/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import LatestNews from '../components/LatestNews';
 import Navbar from '../components/Navbar';
+import LeftNavbar from '../components/layout-component/LeftNavbar';
 
 const HomeLayout = () => {
     return (
@@ -18,7 +19,9 @@ const HomeLayout = () => {
                 </section>
             </nav>
             <main className='w-11/12 mx-auto pt-5 grid md:grid-cols-12'>
-                <aside className='left col-span-3'>Left Navbar</aside>
+                <aside className='left col-span-3'>
+                    <LeftNavbar></LeftNavbar>
+                </aside>
                 <section className='col-span-6'>Main Content</section>
                 <aside className='col-span-3'>Right Navbar</aside>
             </main>
